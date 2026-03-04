1f047008cba4a155c94b937245644c140ea5e960
Darren Liu <dayuanliu11@gmail.com>
2026-03-04 14:02:22 -0500

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,13 +1,36 @@
-const fs = require('fs');
+// const fs = require('fs');
+
+// const dbConfig = {
+//   host: process.env.MySQL_HOST,
+//   user: process.env.MySQL_USER,
+//   password: process.env.MySQL_PASSWORD,
+//   database: process.env.MySQL_DB,
+//   port: process.env.MySQL_PORT,
+//   ssl: { 
+//     ca: fs.readFileSync('DigiCertGlobalRootCA.crt.pem') 
+//   },
+// };
+
+// module.exports = dbConfig;
+
+
+const fs = require("fs");
+const path = require("path");
+
+const caPath = path.join(__dirname, "..", "DigiCertGlobalRootCA.crt.pem"); 
+// __dirname = /home/site/wwwroot/config
+// .. 回到 /home/site/wwwroot (專案根目錄)
 
 const dbConfig = {
   host: process.env.MySQL_HOST,
   user: process.env.MySQL_USER,
   password: process.env.MySQL_PASSWORD,
   database: process.env.MySQL_DB,
-  port: process.env.MySQL_PORT,
-  ssl: { 
-    ca: fs.readFileSync('DigiCertGlobalRootCA.crt.pem') 
+  port: Number(process.env.MySQL_PORT || 3306),
+
+  ssl: {
+    ca: fs.readFileSync(caPath, "utf8"),
+    rejectUnauthorized: true,
   },
 };
 

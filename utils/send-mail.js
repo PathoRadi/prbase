d05378f167f883148b9cfab8adb5d8990f331f54
Darren Liu <dayuanliu11@gmail.com>
2026-06-17 15:10:43 -0400

    update

--- utils/send-mail.js
+++ utils/send-mail.js
@@ -6,45 +6,10 @@
  * @param {string} message - The body content of the email
  * @returns {boolean} - Returns true if the email was sent successfully, otherwise false
  */
-// const nodemailer = require('nodemailer');
-
-// const sendMail = async (from, to, subject, message) => {
-//   try {
-//     const transporter = nodemailer.createTransport({
-//       service: 'gmail',
-//       auth: {
-//         user: process.env.GMAIL_USER,
-//         pass: process.env.GMAIL_PASSWORD,
-//       },
-//     });
-
-//     const mailOptions = {
-//       from: from,
-//       to: to,
-//       subject: subject,
-//       text: message,
-//       html: message,
-//     };
-
-//     const info = await transporter.sendMail(mailOptions);
-//     return true;
-//   } catch (error) {
-//     console.error('Error sending email:', error);
-//     throw new Error('Error sending email');
-//   }
-// };
-
-// module.exports = sendMail;
-
-
 const nodemailer = require('nodemailer');
 
 const sendMail = async (from, to, subject, message) => {
   try {
-    if (!process.env.GMAIL_USER || !process.env.GMAIL_PASSWORD) {
-      throw new Error('Missing GMAIL_USER or GMAIL_PASSWORD in environment variables');
-    }
-
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -54,25 +19,18 @@ const sendMail = async (from, to, subject, message) => {
     });
 
     const mailOptions = {
-      from,
-      to,
-      subject,
+      from: from,
+      to: to,
+      subject: subject,
       text: message,
       html: message,
     };
 
     const info = await transporter.sendMail(mailOptions);
-
-    console.log('Email sent successfully:', {
-      to,
-      subject,
-      messageId: info.messageId,
-    });
-
     return true;
   } catch (error) {
     console.error('Error sending email:', error);
-    throw error;
+    throw new Error('Error sending email');
   }
 };
 
